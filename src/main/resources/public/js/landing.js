01839a37f7b6444327484ba2d2602d824516fe06
Leetine <imlitingting@outlook.com>
2021-11-03 14:33:56 -0500

    index check format

--- src/main/resources/public/js/landing.js
+++ src/main/resources/public/js/landing.js
@@ -9,33 +9,40 @@ window.onload = function() {
 };
 
 function doLogin(){
-    $.post("/login", {username: $("#usernameLogin").val(),
-        password: $("#passwordLogin").val()}, function (data) {
-        console.log(data);
-        // get a boolean - if true then submit, otherwise do not direct AND show error message
-        if (data === true) {
-            localStorage.setItem("username", $("#usernameLogin").val());
-            document.getElementById("registerInfo").submit();
-        }
-    }, "json");
+    checkLogin();
+    if(($("#usernameLogin").val() !== "") && ($("#passwordLogin").val() !== "")) {
+        $.post("/login", {username: $("#usernameLogin").val(),
+            password: $("#passwordLogin").val()}, function (data) {
+            console.log(data);
+            // get a boolean - if true then submit, otherwise do not direct AND show error message
+            if (data === true) {
+                localStorage.setItem("username", $("#usernameLogin").val());
+                document.getElementById("registerInfo").submit();
+            }
+        }, "json");
+    }
 }
 
 function doRegister() {
-    // assume front-end can compare the password with the password confirmation
-    $.post("/register", {username: $("#username").val(), school: $("#school").val(),
-        age: $("#age").val(), interests: $("#interests").val(),
-        password: $("#password").val()}, function (data) {
-        console.log(data);
-        // if true then register successfully, otherwise there exists the same username
-        if (data === true) {
-            console.log("success");
-            // close the form
-            $("#registerForm").modal('hide');
-        } else {
-            console.log("fail");
-            // show error messages on the form
-        }
-    }, "json");
+    checkRegister();
+    if(checkComplete()) {
+        // assume front-end can compare the password with the password confirmation
+        $.post("/register", {username: $("#username").val(), school: $("#school").val(),
+            age: $("#age").val(), interests: $("#interests").val(),
+            password: $("#password").val()}, function (data) {
+            console.log(data);
+            // if true then register successfully, otherwise there exists the same username
+            if (data === true) {
+                console.log("success");
+                // close the form
+                $("#registerForm").modal('hide');
+            } else {
+                console.log("fail");
+                // show error messages on the form
+            }
+        }, "json");
+    }
+
 }
 
 function clear() {
@@ -53,10 +60,158 @@ function clearForm() {
     document.getElementById("interests").value = "";
     document.getElementById("password").value = "";
     document.getElementById("passwordSecond").value = "";
+    document.getElementById("usernameAlert").innerText = "";
+    document.getElementById("schoolAlert").innerText = "";
+    document.getElementById("ageAlert").innerText = "";
+    document.getElementById("interestsAlert").innerText = "";
+    document.getElementById("passwordAlert").innerText = "";
+    document.getElementById("passwordSecondAlert").innerText = "";
 }
 
 
+function validateLoginUserName() {
+    var a = document.getElementById("usernameLogin");
+    var a2 = document.getElementById("usernameLoginAlert");
+    if (a.value === "") {
+        a2.innerText = "!";
+        return false;
+    }
+    else {
+        a2.innerText = "";
+    }
+    return true;
+}
+
+function validateLoginPassword() {
+    var a = document.getElementById("passwordLogin");
+    var a2 = document.getElementById("passwordLoginAlert");
+    if (a.value === "") {
+        a2.innerText = "!";
+        return false;
+    }
+    else {
+        a2.innerText = "";
+    }
+    return true;
+}
+
+function checkLogin() {
+    validateLoginUserName();
+    validateLoginPassword();
+}
+
+function validateRegisterUserName() {
+    var a = document.getElementById("username");
+    var a2 = document.getElementById("usernameAlert");
+    if (a.value === "") {
+        a2.innerText = "!";
+        return false;
+    }
+    else {
+        a2.innerText = ": )";
+    }
+    return true;
+}
+
+function validateRegisterSchool() {
+    var a = document.getElementById("school");
+    var a2 = document.getElementById("schoolAlert");
+    if (a.value === "") {
+        a2.innerText = "!";
+        return false;
+    }
+    else {
+        a2.innerText = ": )";
+    }
+    return true;
+}
+
+function validateRegisterAge() {
+    var a = document.getElementById("age");
+    var a2 = document.getElementById("ageAlert");
+    if (a.value === "") {
+        a2.innerText = "!";
+        return false;
+    }
+    else if(isNaN(a.value)) {
+        a2.innerText = "Age should be integer";
+        document.getElementById("age").value = "";
+        return false;
+    }
+    else if(parseInt(a.value,10) < 18 || parseInt(a.value,10) >= 110) {
+        a2.innerText = "Age from 18 to 110, please";
+        document.getElementById("age").value = "";
+        return false;
+    }
+    else {
+        a2.innerText = ": )";
+    }
+    return true;
+}
+
+function validateRegisterInterests() {
+    var a = document.getElementById("interests");
+    var a1 = document.getElementById("interestsInstr");
+    var a2 = document.getElementById("interestsAlert");
+    if (a.value === "") {
+        a2.innerText = "!";
+        return false;
+    }
+    else if(a.validity.patternMismatch && a.value !== "") {
+        a1.innerText = "";
+        a2.innerText = "Words split by comma";
+    }
+    else {
+        a2.innerText = ": )";
+    }
+    return true;
+}
+
+function validateRegisterPassword() {
+    var pass = document.getElementById("password");
+    var pass2 = document.getElementById("passwordSecond");
+    var alert = document.getElementById("passwordAlert");
+    var alert2 = document.getElementById("passwordSecondAlert");
+    if(pass.value === "") {
+        alert.innerText = "!";
+        return false;
+    }
+    if(pass2.value === "") {
+        alert2.innerText = "!";
+        return false;
+    }
+    if (pass.value !== pass2.value){
+        alert.innerText = "";
+        alert2.innerText = ">_<";
+    }
+    else {
+        alert.innerText = ": )";
+        alert2.innerText = ": )";
+    }
+    return true;
+}
+
+function checkComplete() {
+    if($("#username").val() !== "" &&
+        $("#school").val() !== "" &&
+        $("#age").val() !== "" &&
+        $("#interests").val() !== "" &&
+        ($("#password").val() !== "" && $("#passwordSecond").val() !== "") &&
+        ($("#password").val() === $("#passwordSecond").val()))
+    {
+        return true;
+    }
+}
+function checkRegister() {
+    validateRegisterUserName();
+    validateRegisterSchool();
+    validateRegisterAge();
+    validateRegisterInterests();
+    validateRegisterPassword();
+}
+
 /* Lize - the code below does not have any function, but do not delete them */
+/*
 function validateZipcode(){
     var a = document.getElementById("zipcode");
     var a2 = document.getElementById("zipcodeAlert");
@@ -120,3 +275,4 @@ function updateInfo(){
     validatePassword();
     clear();
 }
+*/
