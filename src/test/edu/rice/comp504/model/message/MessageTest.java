79e45e27852e680e5ee47017e8030917e65817cb
Darren <2443592990@qq.com>
2021-11-05 22:07:17 -0500

    Unit test Daolun

---
+++ src/test/edu/rice/comp504/model/message/MessageTest.java
@@ -0,0 +1,30 @@
+package edu.rice.comp504.model.message;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MessageTest {
+
+    Message message = ImageMessage.make("a","a","a",12);
+    @Test
+    public void getSendUser() {
+        assertEquals("a",message.getSendUser());
+    }
+
+    @Test
+    public void getTimestamp() {
+        assertEquals("a",message.getSendUser());
+    }
+
+    @Test
+    public void getType() {
+        assertEquals("image",message.getType());
+    }
+
+    @Test
+    public void make() {
+        Message message2 = ImageMessage.make("a","a","a",12);
+        assertEquals(message2,message2);
+    }
+}
